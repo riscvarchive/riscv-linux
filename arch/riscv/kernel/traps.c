c82953e7cd08d7f36c6b10f77d5a8250bf9f3b35
Albert Ou <a_ou@eecs.berkeley.edu>
2012-06-30 23:20:23 -0700

    Enable architecture-defined dump_stack()
    dump_stack() is called by panic() if CONFIG_DEBUG_BUGVERBOSE
    is set.  This allows for printing of stack traces in the event
    of kernel panics.

--- arch/riscv/kernel/traps.c
+++ arch/riscv/kernel/traps.c
@@ -1,10 +1,19 @@
+#include <linux/kernel.h>
+#include <linux/export.h>
 #include <linux/init.h>
+
 #include <asm/processor.h>
 #include <asm/ptrace.h>
 
 void show_stack(struct task_struct *task, unsigned long *sp)
 {
 }
+
+void dump_stack(void)
+{
+}
+
+EXPORT_SYMBOL(dump_stack);
 
 void show_regs(struct pt_regs *regs)
 {
