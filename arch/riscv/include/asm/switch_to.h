926864dd45b0794a964564e5bdfa2cb396ba8cc9
Albert Ou <a_ou@eecs.berkeley.edu>
2013-03-01 22:42:26 -0800

    Remove old switch_to.h header

--- arch/riscv/include/asm/switch_to.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ASM_RISCV_SWITCH_TO_H
-#define _ASM_RISCV_SWITCH_TO_H
-
-#include <linux/linkage.h>
-
-extern asmlinkage void switch_to(struct task_struct *prev,
-    struct task_struct *next, struct task_struct *last);
-
-#endif /* _ASM_RISCV_SWITCH_TO */
