9969bcdcaf5753ce1e8f558b21eb414355e65d2f
Albert Ou <aou@eecs.berkeley.edu>
2014-11-11 16:00:00 -0800

    UAPI: Isolate asm headers exported to userspace

--- arch/riscv/include/uapi/asm/unistd.h
+++ arch/riscv/include/uapi/asm/unistd.h
@@ -1,9 +1,3 @@
-#include <asm-generic/unistd.h>
-
-#define __NR_sysriscv  __NR_arch_specific_syscall
-#ifndef __riscv_atomic
-__SYSCALL(__NR_sysriscv, sys_sysriscv)
-#endif
-
-#define RISCV_ATOMIC_CMPXCHG    1
-#define RISCV_ATOMIC_CMPXCHG64  2
+#define __ARCH_HAVE_MMU
+#define __ARCH_WANT_SYS_CLONE
+#include <uapi/asm/unistd.h>
