d84ce5d0acd64c0a4d2de3f66b5cb961409ce122
Albert Ou <aou@eecs.berkeley.edu>
2014-11-11 17:30:00 -0800

    UAPI: Refactor header definitions
    * Prepend `_UAPI' prefix to include guards.
    * Export general-purpose ELF definitions to userspace;
      synchronize relocation types with the current toolchain.
    * Remove unused __NR_ipc macro.

--- arch/riscv/include/uapi/asm/unistd.h
+++ arch/riscv/include/uapi/asm/unistd.h
@@ -1,3 +1,20 @@
 #define __ARCH_HAVE_MMU
+
+#define __ARCH_WANT_SYSCALL_NO_AT
+#define __ARCH_WANT_SYSCALL_DEPRECATED
+#define __ARCH_WANT_SYSCALL_OFF_T
+#define __ARCH_WANT_SYSCALL_NO_FLAGS
+#define __ARCH_WANT_SYS_EXECVE
 #define __ARCH_WANT_SYS_CLONE
-#include <uapi/asm/unistd.h>
+#define __ARCH_WANT_SYS_VFORK
+#define __ARCH_WANT_SYS_FORK
+
+#include <asm-generic/unistd.h>
+
+#define __NR_sysriscv  __NR_arch_specific_syscall
+#ifdef CONFIG_RV_SYSRISCV_ATOMIC
+__SYSCALL(__NR_sysriscv, sys_sysriscv)
+#endif
+
+#define RISCV_ATOMIC_CMPXCHG    1
+#define RISCV_ATOMIC_CMPXCHG64  2
