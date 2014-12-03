bd98b79b96d1b5969070a01e9338e7986bcea797
Darius Rad <darius@bluespec.com>
2014-12-02 19:13:11 -0500

    riscv: Add handler for mmap2 syscall on 32-bit

--- arch/riscv/kernel/sys_riscv.c
+++ arch/riscv/kernel/sys_riscv.c
@@ -10,6 +10,20 @@ SYSCALL_DEFINE6(mmap, unsigned long, addr, unsigned long, len,
 	return sys_mmap_pgoff(addr, len, prot, flags, fd, offset >> PAGE_SHIFT);
 }
 
+#ifndef CONFIG_64BIT
+SYSCALL_DEFINE6(mmap2, unsigned long, addr, unsigned long, len,
+	unsigned long, prot, unsigned long, flags,
+	unsigned long, fd, off_t, offset)
+{
+	/* Note that the shift for mmap2 is constant (12),
+	   regardless of PAGE_SIZE */
+	if (unlikely(offset & (~PAGE_MASK >> 12)))
+		return -EINVAL;
+	return sys_mmap_pgoff(addr, len, prot, flags, fd,
+		offset >> (PAGE_SHIFT - 12));
+}
+#endif /* !CONFIG_64BIT */
+
 #ifdef CONFIG_RV_SYSRISCV_ATOMIC
 SYSCALL_DEFINE4(sysriscv, unsigned long, cmd, unsigned long, arg1,
 	unsigned long, arg2, unsigned long, arg3)
