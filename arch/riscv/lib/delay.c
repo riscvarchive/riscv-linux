20354b26ff5021078c347029453a127415b31635
Albert Ou <aou@eecs.berkeley.edu>
2015-01-28 01:30:41 -0800

    delay: Implement __udelay() and __ndelay()
    These functions are not presently required to build the default kernel,
    but their callers udelay() and ndelay(), defined in <asm-generic/delay.h>,
    appear in many potentially selected drivers.

--- arch/riscv/lib/delay.c
+++ arch/riscv/lib/delay.c
@@ -1,19 +1,36 @@
 #include <linux/delay.h>
 #include <linux/param.h>
 #include <linux/timex.h>
+#include <linux/export.h>
+
+inline void __delay(unsigned long loops)
+{
+	cycles_t start;
+
+	start = get_cycles();
+	do {
+		cpu_relax();
+	} while ((get_cycles() - start) < loops);
+}
+EXPORT_SYMBOL(__delay);
 
 inline void __const_udelay(unsigned long xloops)
 {
 	u64 loops;
+
 	loops = (u64)xloops * loops_per_jiffy * HZ;
 	__delay(loops >> 32);
 }
+EXPORT_SYMBOL(__const_udelay);
 
-inline void __delay(unsigned long loops)
+void __udelay(unsigned long usecs)
 {
-	cycles_t start, now;
-	start = get_cycles();
-	do {
-		now = get_cycles();
-	} while ((now - start) < loops);
+	__const_udelay(usecs * 0x10C7UL); /* 2**32 / 1000000 (rounded up) */
 }
+EXPORT_SYMBOL(__udelay);
+
+void __ndelay(unsigned long nsecs)
+{
+	__const_udelay(nsecs * 0x5UL); /* 2**32 / 1000000000 (rounded up) */
+}
+EXPORT_SYMBOL(__ndelay);
