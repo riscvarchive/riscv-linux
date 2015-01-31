21869ddbaa31007de375368a5c353e2509a55f24
Albert Ou <aou@eecs.berkeley.edu>
2015-01-31 13:44:00 -0800

    unistd: Eliminate __ARCH_WANT_SYSCALL_* legacy
    In addition, remove obsolete __ARCH* macro definitions.  This userspace
    ABI change must be coordinated with a glibc patch to fix the dependence
    on deprecated syscalls.

---
+++ arch/riscv/include/asm/unistd.h
@@ -0,0 +1,3 @@
+#define __ARCH_HAVE_MMU
+#define __ARCH_WANT_SYS_CLONE
+#include <uapi/asm/unistd.h>
--- arch/riscv/include/uapi/asm/unistd.h
+++ arch/riscv/include/uapi/asm/unistd.h
@@ -1,15 +1,3 @@
-#define __ARCH_HAVE_MMU
-
-#define __ARCH_WANT_SYSCALL_NO_AT
-#define __ARCH_WANT_SYSCALL_DEPRECATED
-#define __ARCH_WANT_SYSCALL_OFF_T
-#define __ARCH_WANT_SYSCALL_NO_FLAGS
-
-#define __ARCH_WANT_SYS_EXECVE
-#define __ARCH_WANT_SYS_CLONE
-#define __ARCH_WANT_SYS_VFORK
-#define __ARCH_WANT_SYS_FORK
-
 #include <asm-generic/unistd.h>
 
 #define __NR_sysriscv  __NR_arch_specific_syscall
