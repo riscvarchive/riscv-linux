60b94bff7152d167f8a23abbd9c5a763efd82b9b
Andrew Waterman <waterman@cs.berkeley.edu>
2015-07-05 15:50:32 -0700

    Align asm functions to 4 bytes, not 2
    Even if RVC is supported, the entry point must be 4-byte aligned.

--- arch/riscv/include/asm/linkage.h
+++ arch/riscv/include/asm/linkage.h
@@ -1,7 +1,7 @@
 #ifndef _ASM_RISCV_LINKAGE_H
 #define _ASM_RISCV_LINKAGE_H
 
-#define __ALIGN		.balign 2
-#define __ALIGN_STR	".balign 2"
+#define __ALIGN		.balign 4
+#define __ALIGN_STR	".balign 4"
 
 #endif /* _ASM_RISCV_LINKAGE_H */
