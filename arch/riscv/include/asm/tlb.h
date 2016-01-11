336b4c32c5eebeb05c0b164046cc4882e036d946
Albert Ou <aou@eecs.berkeley.edu>
2016-01-11 15:27:15 -0800

    Migrate to the 4.1 longterm branch (5/8)
    * Use generic tlb_start_vma(), tlb_end_vma(), and __tlb_remove_tlb_entry().
    	"mmu_gather: move minimal range calculations into generic code"
    		fb7332a9fedfd62b1ba6530c86f39f0fa38afd49

--- arch/riscv/include/asm/tlb.h
+++ arch/riscv/include/asm/tlb.h
@@ -8,20 +8,4 @@ static inline void tlb_flush(struct mmu_gather *tlb)
 	flush_tlb_mm(tlb->mm);
 }
 
-static inline void tlb_start_vma(struct mmu_gather *tlb,
-	struct vm_area_struct *vma)
-{
-}
-
-static inline void tlb_end_vma(struct mmu_gather *tlb,
-	struct vm_area_struct *vma)
-{
-}
-
-/* Remove TLB entry for PTE mapped at a given virtual address */
-static inline void __tlb_remove_tlb_entry(struct mmu_gather *tlb,
-	pte_t *ptep, unsigned long addr)
-{
-}
-
 #endif /* _ASM_RISCV_TLB_H */
