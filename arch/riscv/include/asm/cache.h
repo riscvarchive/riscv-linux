c15dbd72f063e633216fc940e74bf165c0e48c70
Andrew Waterman <andrew@sifive.com>
2017-02-21 17:23:44 -0800

    Remove Rocket core configuration for now
    It wasn't doing anything besides setting the cache line size, but
    since Rocket is a generator, even that was not necessarily correct.
    For now, assume a 64-byte cache line across the board, which will
    avoid false sharing for nearly all systems.
    
    This can be fine-tuned from config string information.

--- arch/riscv/include/asm/cache.h
+++ arch/riscv/include/asm/cache.h
@@ -1,11 +1,7 @@
 #ifndef _ASM_RISCV_CACHE_H
 #define _ASM_RISCV_CACHE_H
 
-#if defined(CONFIG_CPU_RV_ROCKET)
 #define L1_CACHE_SHIFT		6
-#else
-#define L1_CACHE_SHIFT		5
-#endif
 
 #define L1_CACHE_BYTES		(1 << L1_CACHE_SHIFT)
 
