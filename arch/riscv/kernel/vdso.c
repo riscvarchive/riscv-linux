8ce407aa316736a7798201710fd508cba88ae257
Palmer Dabbelt <palmer@dabbelt.com>
2017-04-26 17:47:30 -0700

    Fix vdso.c copyright header, it come from ARM

--- arch/riscv/kernel/vdso.c
+++ arch/riscv/kernel/vdso.c
@@ -1,12 +1,20 @@
 /*
- *    Copyright (C) 2004 Benjamin Herrenschmidt, IBM Corp.
- *                       <benh@kernel.crashing.org>
- *    Copyright (C) 2014 Regents of the University of California
+ * Copyright (C) 2004 Benjamin Herrenschmidt, IBM Corp.
+ *                    <benh@kernel.crashing.org>
+ * Copyright (C) 2012 ARM Limited
+ * Copyright (C) 2015 Regents of the University of California
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version
- *  2 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <linux/mm.h>
