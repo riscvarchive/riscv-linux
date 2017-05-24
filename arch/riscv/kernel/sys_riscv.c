54699fb82cb7c34603fb1ec00f4e1f3c2c741e2d
Palmer Dabbelt <palmer@dabbelt.com>
2017-05-23 18:22:06 -0700

    Fix license headers

--- arch/riscv/kernel/sys_riscv.c
+++ arch/riscv/kernel/sys_riscv.c
@@ -6,11 +6,10 @@
  *   modify it under the terms of the GNU General Public License
  *   as published by the Free Software Foundation, version 2.
  *
- *   This program is distributed in the hope that it will be useful, but
- *   WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
- *   NON INFRINGEMENT.  See the GNU General Public License for
- *   more details.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
  */
 
 #include <linux/syscalls.h>
--- arch/riscv/mm/extable.c
+++ arch/riscv/mm/extable.c
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see the file COPYING, or write
  * to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 
