4ac738303c61ab39d3ac4cfdfe9e66fbd6e49c2e
Palmer Dabbelt <palmer@dabbelt.com>
2017-06-05 15:14:11 -0700

    fixup: pci bios

--- drivers/pci/bios.c
+++ drivers/pci/bios.c
@@ -36,7 +36,7 @@ void pcibios_fixup_bus(struct pci_bus *bus)
  */
 __attribute__ ((weak))
 resource_size_t pcibios_align_resource(void *data, const struct resource *res,
-                               resource_size_t size, resource_size_t align)
+				       resource_size_t size, resource_size_t align)
 {
        return res->start;
 }
