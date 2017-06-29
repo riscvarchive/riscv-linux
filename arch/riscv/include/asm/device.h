e3f396a0f35f123492a775e46fd16825a0d50b0f
Palmer Dabbelt <palmer@dabbelt.com>
2017-06-29 15:50:26 -0700

    Use asm-generic/device.h

--- arch/riscv/include/asm/device.h
+++ arch/riscv/include/asm/device.h
@@ -11,17 +11,9 @@
  *   GNU General Public License for more details.
  */
 
-
 #ifndef _ASM_RISCV_DEVICE_H
 #define _ASM_RISCV_DEVICE_H
 
-#include <linux/sysfs.h>
-
-struct dev_archdata {
-	struct dma_map_ops *dma_ops;
-};
-
-struct pdev_archdata {
-};
+#include <asm-generic/device.h>
 
 #endif /* _ASM_RISCV_DEVICE_H */
