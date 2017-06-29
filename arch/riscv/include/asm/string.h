88ae0fc8c929d49a8ba90e6b3f958b032e0a2e66
Palmer Dabbelt <palmer@dabbelt.com>
2017-06-29 15:11:24 -0700

    arch/riscv/include/asm/string.h

--- arch/riscv/include/asm/string.h
+++ arch/riscv/include/asm/string.h
@@ -14,8 +14,6 @@
 #ifndef _ASM_RISCV_STRING_H
 #define _ASM_RISCV_STRING_H
 
-#ifdef __KERNEL__
-
 #include <linux/types.h>
 #include <linux/linkage.h>
 
@@ -24,7 +22,5 @@ extern asmlinkage void *memset(void *, int, size_t);
 
 #define __HAVE_ARCH_MEMCPY
 extern asmlinkage void *memcpy(void *, const void *, size_t);
-
-#endif /* __KERNEL__ */
 
 #endif /* _ASM_RISCV_STRING_H */
