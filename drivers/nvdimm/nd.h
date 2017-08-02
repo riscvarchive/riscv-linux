05ad4556869b3d0b7d120ca3ae3a8acf3e454c28
Andrew Morton <akpm@linux-foundation.org>
2017-08-02 10:37:53 +1000

    nvdimm-avoid-bogus-wmaybe-uninitialized-warning-fix
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Christoph Hellwig <hch@infradead.org>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Johannes Thumshirn <jthumshirn@suse.de>
    Cc: Toshi Kani <toshi.kani@hpe.com>
    Cc: Vishal Verma <vishal.l.verma@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- drivers/nvdimm/nd.h
+++ drivers/nvdimm/nd.h
@@ -393,7 +393,7 @@ static inline bool nd_iostat_start(struct bio *bio, unsigned long *start)
 	struct gendisk *disk = bio->bi_bdev->bd_disk;
 
 	if (!blk_queue_io_stat(disk->queue)) {
-		*start = 0;
+		*start = 0;	/* Suppress bogus warning */
 		return false;
 	}
 
