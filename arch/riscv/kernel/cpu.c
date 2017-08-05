57b03edf2c3fc60b7ab200b3ae40a1027d61fb2c
Palmer Dabbelt <palmer@dabbelt.com>
2017-08-05 14:39:19 -0700

    Be more verbose about bad hard warnings

--- arch/riscv/kernel/cpu.c
+++ arch/riscv/kernel/cpu.c
@@ -21,18 +21,37 @@ int riscv_of_processor_hart(struct device_node *node)
 	const char *isa, *status;
 	u32 hart;
 
-	if (!of_device_is_compatible(node, "riscv"))
+	if (!of_device_is_compatible(node, "riscv")) {
+		pr_warn("Found incompatible CPU\n");
 		return -(ENODEV);
-	if (of_property_read_u32(node, "reg", &hart)
-	    || hart >= NR_CPUS)
+	}
+
+	if (of_property_read_u32(node, "reg", &hart)) {
+		pr_warn("Found CPU without hart ID\n");
 		return -(ENODEV);
-	if (of_property_read_string(node, "status", &status)
-	    || strcmp(status, "okay"))
+	}
+	if (hart >= NR_CPUS) {
+		pr_info("Found hart ID %d, which is above NR_CPUs.  Disabling this hart\n", hart);
 		return -(ENODEV);
-	if (of_property_read_string(node, "riscv,isa", &isa)
-	    || isa[0] != 'r'
-	    || isa[1] != 'v')
+	}
+
+	if (of_property_read_string(node, "status", &status)) {
+		pr_warn("CPU with hartid=%d has no \"status\" property\n", hart);
 		return -(ENODEV);
+	}
+	if (strcmp(status, "okay")) {
+		pr_info("CPU with hartid=%d has a non-okay status of \"%s\"\n", hart, status);
+		return -(ENODEV);
+	}
+
+	if (of_property_read_string(node, "riscv,isa", &isa)) {
+		pr_warn("CPU with hartid=%d has no \"riscv,isa\" property\n", hart);
+		return -(ENODEV);
+	}
+	if (isa[0] != 'r' || isa[1] != 'v') {
+		pr_warn("CPU with hartid=%d has an invalid ISA of \"%s\"\n", hart, isa);
+		return -(ENODEV);
+	}
 
 	return hart;
 }
