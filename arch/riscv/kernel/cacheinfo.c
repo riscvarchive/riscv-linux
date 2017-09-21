64637e8cdc99c63dc6616eec75fa91c699f6fc69
Wesley W. Terpstra <wesley@sifive.com>
2017-09-21 16:02:11 -0700

    riscv cacheinfo: support multiple next-level-caches

--- arch/riscv/kernel/cacheinfo.c
+++ arch/riscv/kernel/cacheinfo.c
@@ -20,7 +20,7 @@ static void ci_leaf_init(struct cacheinfo *this_leaf,
 			 struct device_node *node,
 			 enum cache_type type, unsigned int level)
 {
-	this_leaf->of_node = node;
+	this_leaf->of_node = of_node_get(node);
 	this_leaf->level = level;
 	this_leaf->type = type;
 	/* not a sector cache */
@@ -32,11 +32,37 @@ static void ci_leaf_init(struct cacheinfo *this_leaf,
 		| CACHE_WRITE_ALLOCATE;
 }
 
+static void init_cache_level_helper(struct device_node *np, int *levels, int *leaves)
+{
+	struct of_phandle_iterator it;
+	int rc, level;
+
+	of_for_each_phandle(&it, rc, np, "next-level-cache", NULL, 0) {
+		if (!of_device_is_compatible(it.node, "cache"))
+			continue;
+		if (of_property_read_u32(it.node, "cache-level", &level))
+			continue;
+		if (level > *levels) *levels = level;
+		if (of_property_read_bool(it.node, "cache-size")) {
+			++*leaves;
+			init_cache_level_helper(it.node, levels, leaves);
+		}
+		if (of_property_read_bool(it.node, "i-cache-size")) {
+			++*leaves;
+			init_cache_level_helper(it.node, levels, leaves);
+		}
+		if (of_property_read_bool(it.node, "d-cache-size")) {
+			++*leaves;
+			init_cache_level_helper(it.node, levels, leaves);
+		}
+	}
+}
+
 static int __init_cache_level(unsigned int cpu)
 {
 	struct cpu_cacheinfo *this_cpu_ci = get_cpu_cacheinfo(cpu);
 	struct device_node *np = of_cpu_device_node_get(cpu);
-	int levels = 0, leaves = 0, level;
+	int levels = 0, leaves = 0;
 
 	if (of_property_read_bool(np, "cache-size"))
 		++leaves;
@@ -47,25 +73,38 @@ static int __init_cache_level(unsigned int cpu)
 	if (leaves > 0)
 		levels = 1;
 
-	while ((np = of_find_next_cache_node(np))) {
-		if (!of_device_is_compatible(np, "cache"))
-			break;
-		if (of_property_read_u32(np, "cache-level", &level))
-			break;
-		if (level <= levels)
-			break;
-		if (of_property_read_bool(np, "cache-size"))
-			++leaves;
-		if (of_property_read_bool(np, "i-cache-size"))
-			++leaves;
-		if (of_property_read_bool(np, "d-cache-size"))
-			++leaves;
-		levels = level;
-	}
+	init_cache_level_helper(np, &levels, &leaves);
 
 	this_cpu_ci->num_levels = levels;
 	this_cpu_ci->num_leaves = leaves;
 	return 0;
+}
+
+static struct cacheinfo *populate_cache_leaves_helper(struct device_node *np, struct cacheinfo *this_leaf)
+{
+	struct of_phandle_iterator it;
+	int rc, level;
+
+	of_for_each_phandle(&it, rc, np, "next-level-cache", NULL, 0) {
+		if (!of_device_is_compatible(it.node, "cache"))
+			continue;
+		if (of_property_read_u32(it.node, "cache-level", &level))
+			continue;
+		if (of_property_read_bool(it.node, "cache-size")) {
+			ci_leaf_init(this_leaf++, it.node, CACHE_TYPE_UNIFIED, level);
+			this_leaf = populate_cache_leaves_helper(it.node, this_leaf);
+		}
+		if (of_property_read_bool(it.node, "i-cache-size")) {
+			ci_leaf_init(this_leaf++, it.node, CACHE_TYPE_INST, level);
+			this_leaf = populate_cache_leaves_helper(it.node, this_leaf);
+		}
+		if (of_property_read_bool(it.node, "d-cache-size")) {
+			ci_leaf_init(this_leaf++, it.node, CACHE_TYPE_DATA, level);
+			this_leaf = populate_cache_leaves_helper(it.node, this_leaf);
+		}
+	}
+
+	return this_leaf;
 }
 
 static int __populate_cache_leaves(unsigned int cpu)
@@ -73,7 +112,7 @@ static int __populate_cache_leaves(unsigned int cpu)
 	struct cpu_cacheinfo *this_cpu_ci = get_cpu_cacheinfo(cpu);
 	struct cacheinfo *this_leaf = this_cpu_ci->info_list;
 	struct device_node *np = of_cpu_device_node_get(cpu);
-	int levels = 1, level = 1;
+	int level = 1;
 
 	if (of_property_read_bool(np, "cache-size"))
 		ci_leaf_init(this_leaf++, np, CACHE_TYPE_UNIFIED, level);
@@ -82,22 +121,7 @@ static int __populate_cache_leaves(unsigned int cpu)
 	if (of_property_read_bool(np, "d-cache-size"))
 		ci_leaf_init(this_leaf++, np, CACHE_TYPE_DATA, level);
 
-	while ((np = of_find_next_cache_node(np))) {
-		if (!of_device_is_compatible(np, "cache"))
-			break;
-		if (of_property_read_u32(np, "cache-level", &level))
-			break;
-		if (level <= levels)
-			break;
-		if (of_property_read_bool(np, "cache-size"))
-			ci_leaf_init(this_leaf++, np, CACHE_TYPE_UNIFIED, level);
-		if (of_property_read_bool(np, "i-cache-size"))
-			ci_leaf_init(this_leaf++, np, CACHE_TYPE_INST, level);
-		if (of_property_read_bool(np, "d-cache-size"))
-			ci_leaf_init(this_leaf++, np, CACHE_TYPE_DATA, level);
-		levels = level;
-	}
-
+	this_leaf = populate_cache_leaves_helper(np, this_leaf);
 	return 0;
 }
 
