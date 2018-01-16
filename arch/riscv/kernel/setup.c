9400ce86cfa4180b7ca5593eb656651ff9ac3b1e
Christoph Hellwig <hch@lst.de>
2018-01-16 09:37:50 +0100

    riscv: add ZONE_DMA32
    This patch allows devices that require memory that can be addressed
    using 32-bit addresses to work easily on RISC-V systems.  The newly
    improved dma-direct ops will tap into this pool automatically for
    32-bit addressing.
    
    Based on an earlier patch from Wesley W. Terpstra.
    
    CC: Wesley W. Terpstra <terpstra@sifive.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

--- arch/riscv/kernel/setup.c
+++ arch/riscv/kernel/setup.c
@@ -171,7 +171,7 @@ static void __init setup_bootmem(void)
 	BUG_ON(mem_size == 0);
 
 	set_max_mapnr(PFN_DOWN(mem_size));
-	max_low_pfn = pfn_base + PFN_DOWN(mem_size);
+	max_low_pfn = memblock_end_of_DRAM();
 
 #ifdef CONFIG_BLK_DEV_INITRD
 	setup_initrd();
@@ -181,6 +181,15 @@ static void __init setup_bootmem(void)
 	early_init_fdt_scan_reserved_mem();
 	memblock_allow_resize();
 	memblock_dump_all();
+
+	for_each_memblock(memory, reg) {
+		unsigned long start_pfn = memblock_region_memory_base_pfn(reg);
+		unsigned long end_pfn = memblock_region_memory_end_pfn(reg);
+
+		memblock_set_node(PFN_PHYS(start_pfn),
+		                  PFN_PHYS(end_pfn - start_pfn),
+		                  &memblock.memory, 0);
+	}
 }
 
 void __init setup_arch(char **cmdline_p)
