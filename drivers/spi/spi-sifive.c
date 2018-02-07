b30523a7cbc2697beb4aedbd426024c99b5938dd
Wesley W. Terpstra <wesley@sifive.com>
2018-02-06 16:31:38 -0800

    spi-sifive: rewrite to handle chip select properly

--- drivers/spi/spi-sifive.c
+++ drivers/spi/spi-sifive.c
@@ -17,7 +17,6 @@
 #include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/spi/spi.h>
-#include <linux/spi/spi_bitbang.h>
 #include <linux/io.h>
 #include <linux/log2.h>
 
@@ -74,205 +73,183 @@
 #define XSPI_RXWM_INTR          0x2
 
 struct sifive_spi {
-	/* bitbang has to be first */
-	struct spi_bitbang bitbang;
-	struct completion done;
-	void __iomem	*regs;	/* virt. address of the control registers */
-
-	int		irq;
-
-	u8 *rx_ptr;		/* pointer in the Tx buffer */
-	const u8 *tx_ptr;	/* pointer in the Rx buffer */
-	struct clk *clk;	/* bus clock */
-	int buffer_size;	/* buffer size in words */
-	u32 cs_inactive;	/* Level of the CS pins when inactive*/
-	unsigned int (*read_fn)(void __iomem *);
-	void (*write_fn)(u32, void __iomem *);
+	void __iomem	*regs;		/* virt. address of the control registers */
+	struct clk	*clk;		/* bus clock */
+	int		irq;		/* watermark irq */
+	int 		buffer_size;	/* buffer size in words */
+	u32		cs_inactive;	/* Level of the CS pins when inactive*/
+	struct completion done;		/* Wake-up from interrupt */
 };
 
-static void xspi_write32(u32 val, void __iomem *addr)
+static void sifive_spi_write(struct sifive_spi *spi, int offset, u32 value)
 {
-	iowrite32(val, addr);
+	iowrite32(value, spi->regs + offset);
 }
 
-static unsigned int xspi_read32(void __iomem *addr)
+static u32 sifive_spi_read(struct sifive_spi *spi, int offset)
 {
-	return ioread32(addr);
+	return ioread32(spi->regs + offset);
 }
 
-static void sifive_spi_tx(struct sifive_spi *xspi)
+static void sifive_spi_init(struct sifive_spi *spi)
 {
-	if (!xspi->tx_ptr) {
-		xspi->write_fn(0, xspi->regs + XSPI_TXDR_OFFSET);
+	/* Watermark interrupts are disabled by default */
+	sifive_spi_write(spi, XSPI_IER_OFFSET, 0);
+
+	/* Default watermark FIFO threshold values */
+	sifive_spi_write(spi, XSPI_TXWMR_OFFSET, 1);
+	sifive_spi_write(spi, XSPI_RXWMR_OFFSET, 0);
+
+	/* !!! Program any CS/SCK Delays and Inactive Time */
+
+	/* Exit specialized memory-mapped SPI flash mode */
+	sifive_spi_write(spi, XSPI_FCTRL_OFFSET, 0);
+}
+
+static void sifive_spi_prep_device(struct sifive_spi *spi, struct spi_device *device)
+{
+	u32 cr;
+
+	/* Update the chip select polarity */
+	if (device->mode & SPI_CS_HIGH)
+		spi->cs_inactive &= ~BIT(device->chip_select);
+	else
+		spi->cs_inactive |= BIT(device->chip_select);
+	sifive_spi_write(spi, XSPI_CSDR_OFFSET, spi->cs_inactive);
+
+	/* Select the correct device */
+	sifive_spi_write(spi, XSPI_CSIDR_OFFSET, device->chip_select);
+
+	/* Switch clock mode bits */
+	cr = sifive_spi_read(spi, XSPI_SCMR_OFFSET) & ~XSPI_SCM_MODE_MASK;
+	if (device->mode & SPI_CPHA)
+	  cr |= XSPI_SCM_CPHA;
+	if (device->mode & SPI_CPOL)
+	  cr |= XSPI_SCM_CPOL;
+	sifive_spi_write(spi, XSPI_SCMR_OFFSET, cr);
+
+	/* Switch format mode bits */
+	cr = sifive_spi_read(spi, XSPI_FFR_OFFSET) & ~XSPI_FF_LSB_FIRST;
+	if (device->mode & SPI_LSB_FIRST)
+	  cr |= XSPI_FF_LSB_FIRST;
+	sifive_spi_write(spi, XSPI_FFR_OFFSET, cr);
+}
+
+static void sifive_spi_prep_transfer(struct sifive_spi *spi, struct spi_device *device, struct spi_transfer *t)
+{
+	u32 hz, scale;
+
+	/* Calculate and program the clock rate */
+	hz = t->speed_hz ? t->speed_hz : device->max_speed_hz;
+	scale = (DIV_ROUND_UP(clk_get_rate(spi->clk), 2 * hz) - 1) & XSPI_SCD_SCALE_MASK;
+	sifive_spi_write(spi, XSPI_SCDR_OFFSET, scale);
+}
+
+static void sifive_spi_tx(struct sifive_spi *spi, const u8* tx_ptr)
+{
+	if (!tx_ptr) {
+		sifive_spi_write(spi, XSPI_TXDR_OFFSET, 0);
 		return;
 	}
 
-	BUG_ON((xspi->read_fn(xspi->regs + XSPI_TXDR_OFFSET) & XSPI_TXD_FIFO_FULL) != 0);
-
-	xspi->write_fn(*(u8 *)(xspi->tx_ptr) & XSPI_DATA_MASK, xspi->regs + XSPI_TXDR_OFFSET);
-	++xspi->tx_ptr;
+	BUG_ON((sifive_spi_read(spi, XSPI_TXDR_OFFSET) & XSPI_TXD_FIFO_FULL) != 0);
+	sifive_spi_write(spi, XSPI_TXDR_OFFSET, *tx_ptr & XSPI_DATA_MASK);
 }
 
-static void sifive_spi_rx(struct sifive_spi *xspi)
+static void sifive_spi_rx(struct sifive_spi *spi, u8* rx_ptr)
 {
-        u32 data = xspi->read_fn(xspi->regs + XSPI_RXDR_OFFSET);
+        u32 data = sifive_spi_read(spi, XSPI_RXDR_OFFSET);
         BUG_ON((data & XSPI_RXD_FIFO_EMPTY) != 0);
 
-	if (!xspi->rx_ptr)
+	if (!rx_ptr)
 		return;
 
-        *(u8 *)(xspi->rx_ptr) = data & XSPI_DATA_MASK;
-	++xspi->rx_ptr;
+        *rx_ptr = data & XSPI_DATA_MASK;
 }
 
-static void xspi_init_hw(struct sifive_spi *xspi)
+static void sifive_spi_execute(struct sifive_spi *spi, struct spi_transfer *t)
 {
-	void __iomem *regs_base = xspi->regs;
+	int remaining_words = t->len;
+	const u8* tx_ptr = t->tx_buf;
+	u8* rx_ptr = t->rx_buf;
 
-	/* Watermark interrupts are disabled by default */
-	xspi->write_fn(0, regs_base + XSPI_IER_OFFSET);
+	while (remaining_words) {
+		int n_words, tx_words, rx_words;
+		n_words = min(remaining_words, spi->buffer_size);
 
-	/* Default watermark FIFO threshold values */
-	xspi->write_fn(1, regs_base + XSPI_TXWMR_OFFSET);
-	xspi->write_fn(0, regs_base + XSPI_RXWMR_OFFSET);
+		/* Enqueue n_words for transmission */
+		for (tx_words = 0; tx_words < n_words; ++tx_words) {
+			sifive_spi_tx(spi, tx_ptr);
+			if (tx_ptr) ++tx_ptr;
+		}
 
-	/* Programe any CS/SCK Delays and Inactive Time*/
-        //xspi->write_fn(0x10001, regs_base + XSPI_DCR0_OFFSET);
-        //xspi->write_fn(0x1, regs_base + XSPI_DCR1_OFFSET);
+		/* Wait for transmission + reception to complete */
+		reinit_completion(&spi->done);
+		sifive_spi_write(spi, XSPI_RXWMR_OFFSET, n_words-1);
+		sifive_spi_write(spi, XSPI_IER_OFFSET, XSPI_RXWM_INTR);
+		wait_for_completion(&spi->done);
 
-	/* Exit specialized memory-mapped SPI flash mode */
-	xspi->write_fn(0, regs_base + XSPI_FCTRL_OFFSET);
+		/* Read out all the data from the Rx FIFO */
+		for (rx_words = 0; rx_words < n_words; ++rx_words) {
+			sifive_spi_rx(spi, rx_ptr);
+			if (rx_ptr) ++rx_ptr;
+		}
+
+		remaining_words -= n_words;
+	}
 }
 
-static void sifive_spi_chipselect(struct spi_device *spi, int is_on)
+static int sifive_spi_transfer_one_message(struct spi_master *master, struct spi_message *msg)
 {
-	struct sifive_spi *xspi = spi_master_get_devdata(spi->master);
+	struct sifive_spi *spi = spi_master_get_devdata(master);
+	struct spi_device *device = msg->spi;
+	struct spi_transfer *t;
+	int is_last, cs_change, cs_enable;
 
-	if (is_on == BITBANG_CS_INACTIVE) return;
+	/* Setup device-specific properties */
+	sifive_spi_prep_device(spi, device);
 
-	/* Activate the chip select */
-	xspi->write_fn(spi->chip_select, xspi->regs + XSPI_CSIDR_OFFSET);
-}
+	sifive_spi_write(spi, XSPI_CSMR_OFFSET, XSPI_CSM_MODE_HOLD);
+	list_for_each_entry(t, &msg->transfers, transfer_list) {
+		/* Setup transfer-specific properties */
+		sifive_spi_prep_transfer(spi, device, t);
+		sifive_spi_execute(spi, t);
+		msg->actual_length += t->len;
 
-/* spi_bitbang requires custom setup_transfer() to be defined if there is a
- * custom txrx_bufs().
- */
-static int sifive_spi_setup_transfer(struct spi_device *spi,
-		struct spi_transfer *t)
-{
-	struct sifive_spi *xspi = spi_master_get_devdata(spi->master);
+		is_last = list_is_last(&t->transfer_list, &msg->transfers);
+		cs_change = !!t->cs_change;
+		cs_enable = !is_last ^ cs_change;
+		sifive_spi_write(spi, XSPI_CSMR_OFFSET, cs_enable ? XSPI_CSM_MODE_HOLD : XSPI_CSM_MODE_AUTO);
+	}
 
-	if (spi->mode & SPI_CS_HIGH)
-		xspi->cs_inactive &= ~BIT(spi->chip_select);
-	else
-		xspi->cs_inactive |= BIT(spi->chip_select);
+	msg->status = 0;
+	spi_finalize_current_message(master);
 
 	return 0;
 }
 
-static int sifive_spi_txrx_bufs(struct spi_device *spi, struct spi_transfer *t)
-{
-	struct sifive_spi *xspi = spi_master_get_devdata(spi->master);
-	int remaining_words;	/* the number of words left to transfer */
-	u32 cr = 0;
-	u8 bpw = 0;
-	u32 hz = 0;
-	int scale;
-
-	/* Calculate and program the clock rate */
-	if (t) {
-	  bpw = t->bits_per_word;
-	  hz = t->speed_hz;
-	}
-	hz = hz ? : spi->max_speed_hz;
-	scale = (DIV_ROUND_UP(clk_get_rate(xspi->clk), 2 * hz) - 1) & XSPI_SCD_SCALE_MASK;
-	xspi->write_fn(scale, xspi->regs + XSPI_SCDR_OFFSET);
-
-	/* Set the SPI clock phase and polarity */
-	cr = xspi->read_fn(xspi->regs + XSPI_SCMR_OFFSET) & ~XSPI_SCM_MODE_MASK;
-	if (spi->mode & SPI_CPHA)
-	  cr |= XSPI_SCM_CPHA;
-	if (spi->mode & SPI_CPOL)
-	  cr |= XSPI_SCM_CPOL;
-	xspi->write_fn(cr, xspi->regs + XSPI_SCMR_OFFSET);
-
-	cr = xspi->read_fn(xspi->regs + XSPI_FFR_OFFSET) & ~XSPI_FF_LSB_FIRST;
-	if (spi->mode & SPI_LSB_FIRST)
-	  cr |= XSPI_FF_LSB_FIRST;
-	xspi->write_fn(cr, xspi->regs + XSPI_FFR_OFFSET);
-
-	xspi->tx_ptr = t->tx_buf;
-	xspi->rx_ptr = t->rx_buf;
-	remaining_words = t->len;
-
-	/* RXM and TXM interrupts must already be disabled */
-	cr = xspi->read_fn(xspi->regs + XSPI_IER_OFFSET);
-	BUG_ON((cr & (XSPI_TXWM_INTR|XSPI_RXWM_INTR)) != 0);
-
-	/* Don't lower CS mid-message */
-	xspi->write_fn(XSPI_CSM_MODE_HOLD, xspi->regs + XSPI_CSMR_OFFSET);
-
-	while (remaining_words) {
-		int n_words, tx_words, rx_words;
-
-		n_words = min(remaining_words, xspi->buffer_size);
-
-		/* Enqueue n_words for transmission */
-		for (tx_words = 0; tx_words < n_words; ++tx_words)
-			sifive_spi_tx(xspi);
-
-		/* Wait for transmission + reception to complete */
-		reinit_completion(&xspi->done);
-		xspi->write_fn(n_words-1, xspi->regs + XSPI_RXWMR_OFFSET);
-		xspi->write_fn(XSPI_RXWM_INTR, xspi->regs + XSPI_IER_OFFSET);
-		wait_for_completion(&xspi->done);
-
-		/* Read out all the data from the Rx FIFO */
-		for (rx_words = 0; rx_words < n_words; ++rx_words)
-			sifive_spi_rx(xspi);
-
-		remaining_words -= n_words;
-	}
-
-	/* If this is the last transfer in the message and we're done, lower CS */
-	if (t->cs_change)
-		xspi->write_fn(XSPI_CSM_MODE_AUTO, xspi->regs + XSPI_CSMR_OFFSET);
-
-	return t->len;
-}
-
-
-/*
- * Watermark threshold interrupts
- */
 static irqreturn_t sifive_spi_irq(int irq, void *dev_id)
 {
-	struct sifive_spi *xspi = dev_id;
+	struct sifive_spi *spi = dev_id;
 	u32 ip;
 
-	ip = xspi->read_fn(xspi->regs + XSPI_IPR_OFFSET) & (XSPI_TXWM_INTR | XSPI_RXWM_INTR);
+	ip = sifive_spi_read(spi, XSPI_IPR_OFFSET) & (XSPI_TXWM_INTR | XSPI_RXWM_INTR);
 	if (ip != 0) {
 		/* Disable interrupts until next transfer */
-		xspi->write_fn(0, xspi->regs + XSPI_IER_OFFSET);
-		complete(&xspi->done);
+		sifive_spi_write(spi, XSPI_IER_OFFSET, 0);
+		complete(&spi->done);
 		return IRQ_HANDLED;
 	}
 
 	return IRQ_NONE;
 }
 
-static const struct of_device_id sifive_spi_of_match[] = {
-	{ .compatible = "sifive,spi0", },
-	{}
-};
-MODULE_DEVICE_TABLE(of, sifive_spi_of_match);
-
 static int sifive_spi_probe(struct platform_device *pdev)
 {
-	struct sifive_spi *xspi;
+	struct sifive_spi *spi;
 	struct resource *res;
 	int ret, num_cs, bits_per_word = 8;
-	long cs_bits;
+	u32 cs_bits;
 	struct spi_master *master;
 
 	master = spi_alloc_master(&pdev->dev, sizeof(struct sifive_spi));
@@ -281,38 +258,47 @@ static int sifive_spi_probe(struct platform_device *pdev)
 		return -ENOMEM;
 	}
 
-	xspi = spi_master_get_devdata(master);
-	xspi->cs_inactive = 0xffffffff;
-	xspi->bitbang.master = master;
-	xspi->bitbang.chipselect = sifive_spi_chipselect;
-	xspi->bitbang.setup_transfer = sifive_spi_setup_transfer;
-	xspi->bitbang.txrx_bufs = sifive_spi_txrx_bufs;
-	init_completion(&xspi->done);
+	spi = spi_master_get_devdata(master);
+	init_completion(&spi->done);
+	platform_set_drvdata(pdev, master);
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	xspi->regs = devm_ioremap_resource(&pdev->dev, res);
-	if (IS_ERR(xspi->regs)) {
+	spi->regs = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(spi->regs)) {
 		dev_err(&pdev->dev, "Unable to map IO resources\n");
-		ret = PTR_ERR(xspi->regs);
+		ret = PTR_ERR(spi->regs);
 		goto put_master;
 	}
 
-	xspi->clk = devm_clk_get(&pdev->dev, NULL);
-	if (IS_ERR(xspi->clk)) {
+	spi->clk = devm_clk_get(&pdev->dev, NULL);
+	if (IS_ERR(spi->clk)) {
 		dev_err(&pdev->dev, "Unable to find bus clock\n");
-		ret = PTR_ERR(xspi->clk);
+		ret = PTR_ERR(spi->clk);
 		goto put_master;
 	}
 
-	ret = clk_prepare_enable(xspi->clk);
+	spi->irq = platform_get_irq(pdev, 0);
+	if (spi->irq < 0) {
+		dev_err(&pdev->dev, "Unable to find interrupt\n");
+		ret = spi->irq;
+		goto put_master;
+	}
+
+	/* !!! This should be in DTS? */
+	spi->buffer_size = 8;
+
+	/* Spin up the bus clock before hitting registers */
+	ret = clk_prepare_enable(spi->clk);
 	if (ret) {
 		dev_err(&pdev->dev, "Unable to enable bus clock\n");
 		goto put_master;
 	}
 
 	/* probe the number of CS lines */
-	xspi_write32(0xffffffffU, xspi->regs + XSPI_CSDR_OFFSET);
-	cs_bits = ioread32(xspi->regs + XSPI_CSDR_OFFSET);
+	spi->cs_inactive = sifive_spi_read(spi, XSPI_CSDR_OFFSET);
+	sifive_spi_write(spi, XSPI_CSDR_OFFSET, 0xffffffffU);
+	cs_bits = sifive_spi_read(spi, XSPI_CSDR_OFFSET);
+	sifive_spi_write(spi, XSPI_CSDR_OFFSET, spi->cs_inactive);
 	if (!cs_bits) {
 		dev_err(&pdev->dev, "Could not auto probe CS lines\n");
 		ret = -EINVAL;
@@ -326,47 +312,34 @@ static int sifive_spi_probe(struct platform_device *pdev)
 		goto put_master;
 	}
 
-	/* the spi->mode bits understood by this driver: */
-	master->mode_bits = SPI_CPOL | SPI_CPHA | SPI_LSB_FIRST | SPI_CS_HIGH;
-	master->bits_per_word_mask = SPI_BPW_MASK(bits_per_word);
+	/* Define our master */
 	master->bus_num = pdev->id;
-	master->num_chipselect = num_cs;
+	master->mode_bits = SPI_CPOL | SPI_CPHA | SPI_LSB_FIRST | SPI_CS_HIGH;
 	master->dev.of_node = pdev->dev.of_node;
+	master->bits_per_word_mask = SPI_BPW_MASK(bits_per_word);
+	master->num_chipselect = num_cs;
+	master->transfer_one_message = sifive_spi_transfer_one_message;
 
-	xspi->read_fn = xspi_read32;
-	xspi->write_fn = xspi_write32;
+	/* Configure the SPI master hardware */
+	sifive_spi_init(spi);
 
-	/*
-	 * Ensure our default match the implementation
-	 */
-	xspi->cs_inactive &= xspi->read_fn(xspi->regs + XSPI_CSDR_OFFSET);
-	xspi->buffer_size = 8;  /* Both TXWM and RXWM are 3bits wide */
-
-	/* SPI controller initializations => disables interrupts */
-	xspi_init_hw(xspi);
-
-	xspi->irq = platform_get_irq(pdev, 0);
-	if (xspi->irq < 0 && xspi->irq != -ENXIO) {
-		ret = xspi->irq;
-		goto put_master;
-	} else if (xspi->irq >= 0) {
-		/* Register for SPI Interrupt */
-		ret = devm_request_irq(&pdev->dev, xspi->irq, sifive_spi_irq, 0,
-				dev_name(&pdev->dev), xspi);
-		if (ret)
-			goto put_master;
-	}
-
-	dev_info(&pdev->dev, "at 0x%08llX mapped to 0x%p, irq=%d, cs=%d\n",
-		(unsigned long long)res->start, xspi->regs, xspi->irq, master->num_chipselect);
-
-	ret = spi_bitbang_start(&xspi->bitbang);
+	/* Register for SPI Interrupt */
+	ret = devm_request_irq(&pdev->dev, spi->irq, sifive_spi_irq, 0,
+				dev_name(&pdev->dev), spi);
 	if (ret) {
-		dev_err(&pdev->dev, "spi_bitbang_start FAILED\n");
+		dev_err(&pdev->dev, "Unable to bind to interrupt\n");
 		goto put_master;
 	}
 
-	platform_set_drvdata(pdev, master);
+	dev_info(&pdev->dev, "mapped; irq=%d, cs=%d\n",
+		spi->irq, master->num_chipselect);
+
+	ret = devm_spi_register_master(&pdev->dev, master);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "spi_register_master failed\n");
+		goto put_master;
+	}
+
 	return 0;
 
 put_master:
@@ -378,18 +351,20 @@ put_master:
 static int sifive_spi_remove(struct platform_device *pdev)
 {
 	struct spi_master *master = platform_get_drvdata(pdev);
-	struct sifive_spi *xspi = spi_master_get_devdata(master);
-	void __iomem *regs_base = xspi->regs;
-
-	spi_bitbang_stop(&xspi->bitbang);
+	struct sifive_spi *spi = spi_master_get_devdata(master);
 
 	/* Disable all the interrupts just in case */
-	xspi->write_fn(0, regs_base + XSPI_IER_OFFSET);
-
-	spi_master_put(xspi->bitbang.master);
+	sifive_spi_write(spi, XSPI_IER_OFFSET, 0);
+	spi_master_put(master);
 
 	return 0;
 }
+
+static const struct of_device_id sifive_spi_of_match[] = {
+	{ .compatible = "sifive,spi0", },
+	{}
+};
+MODULE_DEVICE_TABLE(of, sifive_spi_of_match);
 
 static struct platform_driver sifive_spi_driver = {
 	.probe = sifive_spi_probe,
