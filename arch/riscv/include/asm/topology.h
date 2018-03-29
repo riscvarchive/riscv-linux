e968a862c0d5424863720d7b2f89c316ac7a4933
Palmer Dabbelt <palmer@sifive.com>
2018-03-28 17:03:45 -0700

    tiny topology

--- arch/riscv/include/asm/topology.h
+++ arch/riscv/include/asm/topology.h
@@ -5,20 +5,7 @@
 
 #include <linux/cpumask.h>
 
-struct cpu_topology {
-	int thread_id;
-	int core_id;
-	int cluster_id;
-	cpumask_t thread_sibling;
-	cpumask_t core_sibling;
-};
-
-extern struct cpu_topology cpu_topology[NR_CPUS];
-
-#define topology_physical_package_id(cpu)	(0)
 #define topology_core_id(cpu)			(cpu)
-#define topology_core_cpumask(cpu)		(1 << cpu)
-#define topology_sibling_cpumask(cpu)		(1 << cpu)
 
 #include <asm-generic/topology.h>
 
